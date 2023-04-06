d89394d62ba28107754598c95c6e49092f9ac4f1
Debabrata Mohapatra <debabratamohapatra47@gmail.com>
2023-04-06 10:53:54 +0530

    files added

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b0be8b981dmsh87fef8152069da4p1e2df0jsn84a1b65f96ff",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="7737396948e0582fbc8b454de944e716"
